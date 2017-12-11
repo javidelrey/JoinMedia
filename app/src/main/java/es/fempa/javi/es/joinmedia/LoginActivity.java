5d8ea0d32849733bae7b506a05e8ed67978c0fc7
Javier del Rey <javidelrey3@gmail.com>
2017-12-11 20:05:09 +0100

    InitialCommit

--- app/src/main/java/es/fempa/javi/es/joinmedia/LoginActivity.java
+++ app/src/main/java/es/fempa/javi/es/joinmedia/LoginActivity.java
@@ -3,6 +3,7 @@ package es.fempa.javi.es.joinmedia;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -49,7 +50,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "join@gmail.com:000000", "media@gmail.com:111111"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -324,7 +325,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
@@ -333,7 +334,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent intent= new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
